aac682849bca910f182c0a6bcc6d79d529b8321f
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2012-11-26 10:30:25 +0100

    Apply user feedback (http://markmail.org/message/llqhotud7fyi33y2)

--- file-upload/src/main/resources/com/mycompany/fileupload/main.js
+++ file-upload/src/main/resources/com/mycompany/fileupload/main.js
@@ -4,7 +4,24 @@ $(function () {
     $('#${componentMarkupId}').fileupload({
         url: '${url}',
         paramName: '${paramName}',
-        singleFileUploads: false
+        singleFileUploads: true,
+        maxFileSize: 5000000,
+        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+        process: [
+            {
+                action: 'load',
+                fileTypes: /^image\/(gif|jpeg|png)$/,
+                maxFileSize: 20000000 // 20MB
+            },
+            {
+                action: 'resize',
+                maxWidth: 1440,
+                maxHeight: 900
+            },
+            {
+                action: 'save'
+            }
+        ]
     });
 
 });
